5a0d6b3c12081022f73a46005d67e6ed5e029242
Sebastian Schildt <sebastian.schildt@de.bosch.com>
2021-02-22 14:58:36 +0100

    Support strict mode in vspec2json
    Signed-off-by: Sebastian Schildt <sebastian.schildt@de.bosch.com>

--- vspec2json.py
+++ vspec2json.py
@@ -19,7 +19,8 @@ from model.vsstree import VSSNode, VSSType
 
 
 def usage():
-    print("Usage:", sys.argv[0], "[-I include_dir] ... [-i prefix:id_file] vspec_file [-s] json_file")
+    print(
+        "Usage:", sys.argv[0], "[-I include_dir] ... [-i prefix:id_file] vspec_file [-s] json_file")
     print("  -s                   Use strict checking: Terminate when non-core attribute is found")
     print("  -I include_dir       Add include directory to search for included vspec")
     print("                       files. Can be used multiple timees.")
@@ -33,70 +34,64 @@ def usage():
     sys.exit(255)
 
 
-
 def export_node(json_dict, node):
 
-    json_dict[node.name]={}
+    json_dict[node.name] = {}
 
     if node.type == VSSType.SENSOR or node.type == VSSType.ACTUATOR or node.type == VSSType.ATTRIBUTE:
-        json_dict[node.name]["datatype"]=str(node.data_type.value)
+        json_dict[node.name]["datatype"] = str(node.data_type.value)
 
-    json_dict[node.name]["type"]=str(node.type.value)
+    json_dict[node.name]["type"] = str(node.type.value)
 
-    #many optional attributes are initilized to "" in vsstree.py
+    # many optional attributes are initilized to "" in vsstree.py
     if node.min != "":
-        json_dict[node.name]["min"]=node.min
+        json_dict[node.name]["min"] = node.min
     if node.max != "":
-        json_dict[node.name]["max"]=node.max
+        json_dict[node.name]["max"] = node.max
     if node.enum != "":
-        json_dict[node.name]["enum"]=node.enum
+        json_dict[node.name]["enum"] = node.enum
     if node.default_value != "":
-        json_dict[node.name]["default"]=node.default_value
+        json_dict[node.name]["default"] = node.default_value
     if node.deprecation != "":
-        json_dict[node.name]["deprecation"]=node.deprecation
+        json_dict[node.name]["deprecation"] = node.deprecation
 
-
-    #in case of unit or aggregate, the attribute will be missing
+    # in case of unit or aggregate, the attribute will be missing
     try:
-        json_dict[node.name]["unit"]=str(node.unit.value)
+        json_dict[node.name]["unit"] = str(node.unit.value)
     except AttributeError:
         pass
     try:
-        json_dict[node.name]["aggregate"]=node.aggregate
+        json_dict[node.name]["aggregate"] = node.aggregate
     except AttributeError:
         pass
 
+    json_dict[node.name]["description"] = node.description
+    json_dict[node.name]["uuid"] = node.uuid
 
-
-    json_dict[node.name]["description"]=node.description
-    json_dict[node.name]["uuid"]=node.uuid
-
-    #Might be better to not generate child dict, if there are no children
-    #if node.type == VSSType.BRANCH and len(node.children) != 0:
+    # Might be better to not generate child dict, if there are no children
+    # if node.type == VSSType.BRANCH and len(node.children) != 0:
     #    json_dict[node.name]["children"]={}
 
-    #But old JSON code always generates children, so lets do so to
+    # But old JSON code always generates children, so lets do so to
     if node.type == VSSType.BRANCH:
-        json_dict[node.name]["children"]={}
-
+        json_dict[node.name]["children"] = {}
 
     for child in node.children:
         export_node(json_dict[node.name]["children"], child)
 
 
-
 def export_json(file, root):
-    json_dict={}
-    export_node(json_dict,root)
-    json.dump(json_dict,file, indent=2, sort_keys=True)
+    json_dict = {}
+    export_node(json_dict, root)
+    json.dump(json_dict, file, indent=2, sort_keys=True)
 
 
 if __name__ == "__main__":
     #
     # Check that we have the correct arguments
     #
-    opts, args= getopt.getopt(sys.argv[1:], "sI:i:")
-    strict=False
+    opts, args = getopt.getopt(sys.argv[1:], "sI:i:")
+    strict = False
 
     # Always search current directory for include_file
     include_dirs = ["."]
@@ -111,21 +106,21 @@ if __name__ == "__main__":
             [prefix, file_name] = id_spec
             vspec.db_mgr.create_signal_uuid_db(prefix, file_name)
         elif o == "-s":
-            strict=True
+            strict = True
         else:
             usage()
 
     if len(args) != 2:
         usage()
 
-    json_out = open (args[1], "w")
-
+    json_out = open(args[1], "w")
 
     try:
         print("Loading vspec...")
-        tree = vspec.load_tree(args[0], include_dirs, exclude_private=False, break_on_noncore_attribute=strict)
+        tree = vspec.load_tree(
+            args[0], include_dirs, exclude_private=False, break_on_noncore_attribute=strict)
         print("Recursing tree and creating JSON...")
-        export_json(json_out,tree)
+        export_json(json_out, tree)
         print("All done.")
     except vspec.VSpecError as e:
         print(f"Error: {e}")
