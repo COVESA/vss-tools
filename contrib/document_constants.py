9abffd2a50d22deecde428d0fd3c271edf950d0c
Gustavo Sverzut Barbieri <barbieri@profusion.mobi>
2021-07-02 14:54:47 -0300

    contrib/document_constants: use VSSConstant
    Use the new VSSConstant to generate tables with domain and
    description.

--- contrib/document_constants.py
+++ contrib/document_constants.py
@@ -11,7 +11,7 @@ import argparse
 import sys
 from enum import Enum
 from textwrap import dedent
-from typing import Mapping, Sequence, Type, Union
+from typing import Callable, List, Sequence
 
 import vspec.model.constants as constants
 
@@ -21,10 +21,112 @@ class GenericEnum(Enum):
     placeholder = 1
 
 
+def get_max_values(a: Sequence[int], b: Sequence[int]) -> Sequence[int]:
+    """Given two sequences, get a sequence with the maximum of each position:
+
+    >>> get_max_values([0, 1, 2], [10, 20, 30])
+    [10, 20, 30]
+    """
+    return [max(a[i], b[i]) for i in range(len(a))]
+
+
+def measure_items(
+    items: Sequence[str],
+    measure: Callable[[str], int],
+) -> Sequence[int]:
+    """Returns a sequence of the measurement for each item in the input:
+
+    >>> measure_items(['a', 'banana'], len)
+    [1, 6]
+    >>> measure_items(['a', 'banana'], lambda text: len(text) * 10)
+    [10, 60]
+    """
+    return [measure(c) for c in items]
+
+
+def get_max_columns_lengths(
+    rows: Sequence[Sequence[str]],
+    columns_count: int,
+    measure: Callable[[str], int]
+) -> Sequence[int]:
+    """Returns the maximum lengths for each column given the rows:
+
+    >>> get_max_columns_lengths(
+    ...    [['a', 'b', 'cccccccccc'],
+    ...     ['apple', 'banana', 'coconut']],
+    ...    3,
+    ...    len)
+    [5, 6, 10]
+    """
+    max_lengths = [0] * columns_count
+    for r in rows:
+        row_lengths = measure_items(r, measure)
+        max_lengths = get_max_values(max_lengths, row_lengths)
+    return max_lengths
+
+
+def create_rounded_str_len(size_rounding: int) -> Callable[[str], int]:
+    """Returns a measurement function that rounds to multiple of given sizes:
+
+    >>> measure4 = create_rounded_str_len(4)
+    >>> measure4('a')
+    4
+    >>> measure4('abcd')
+    4
+    >>> measure4('abcde')
+    8
+
+    >>> measure3 = create_rounded_str_len(3)
+    >>> measure3('a')
+    3
+    >>> measure3('abcd')
+    6
+    >>> measure3('abcde')
+    6
+    """
+    def measure(text: str):
+        n = len(text)
+        remainder = n % size_rounding
+        if not remainder:
+            return n
+        return n + size_rounding - remainder
+    return measure
+
+
+def create_table_format(lengths: Sequence[int]) -> str:
+    """Returns the Python-format to left-align the columns:
+
+    >>> create_table_format([4, 8])
+    '| %-4s | %-8s |'
+    >>> create_table_format([4, 8, 12])
+    '| %-4s | %-8s | %-12s |'
+    """
+    fmt = "|"
+    for length in lengths:
+        fmt += f" %-{length}s |"
+    return fmt
+
+
+def create_table_header_separator(lengths: Sequence[str]) -> str:
+    """Returns the Markdown table separator the columns:
+
+    >>> create_table_header_separator([4, 8])
+    '|:-----|:---------|'
+    >>> create_table_header_separator([4, 8, 12])
+    '|:-----|:---------|:-------------|'
+    """
+    fmt = "|"
+    for length in lengths:
+        dashes = '-' * length
+        fmt += f":{dashes}-|"
+    return fmt
+
+
 def create_table(
     header: Sequence[str],
     rows: Sequence[Sequence[str]],
     size_rounding: int = 4,
+    omit_empty_columns: bool = True,
 ):
     """Generate markdown table
 
@@ -33,36 +135,70 @@ def create_table(
 
     To avoid too much resizing when the cells change, the sizes are rounded
     using `size_rounding`.
+
+    Example:
+    >>> print(create_table(
+    ...   ['key', 'value'],
+    ...   [['a', '1'], ['longer key here', '123456']],
+    ...   4))
+    | key              | value    |
+    |:-----------------|:---------|
+    | a                | 1        |
+    | longer key here  | 123456   |
+
+    Columns with values will be omitted by default:
+    >>> print(create_table(
+    ...   ['key', 'value', 'nothing'],
+    ...   [['a', '1', ''], ['longer key here', '123456', '']],
+    ...   4))
+    | key              | value    |
+    |:-----------------|:---------|
+    | a                | 1        |
+    | longer key here  | 123456   |
+
+    One can force them to be displayed regardless:
+    >>> print(create_table(
+    ...   ['key', 'value', 'nothing'],
+    ...   [['a', '1', ''], ['longer key here', '123456', '']],
+    ...   4, False))
+    | key              | value    | nothing  |
+    |:-----------------|:---------|:---------|
+    | a                | 1        |          |
+    | longer key here  | 123456   |          |
     """
-    def round(n):
-        remainder = n % size_rounding
-        if not remainder:
-            return n
-        return n + remainder
+    measure = create_rounded_str_len(size_rounding)
+    max_lengths = get_max_columns_lengths(
+        rows,
+        len(header),
+        measure,
+    )
+    if not omit_empty_columns:
+        def filter_visible_columns(seq):
+            return tuple(seq)
+    else:
+        visible_columns = [bool(ml) for ml in max_lengths]
+        def filter_visible_columns(seq):
+            return tuple(x for i, x in enumerate(seq) if visible_columns[i])
 
-    max_lengths = [round(len(h)) for h in header]
-    count = len(max_lengths)
-    for r in rows:
-        for i in range(count):
-            max_lengths[i] = max(max_lengths[i], round(len(r[i])))
+    max_lengths = get_max_values(
+        max_lengths,
+        measure_items(header, measure),
+    )
 
-    fmt = "|"
-    for ml in max_lengths:
-        fmt += f" %-{ml}s |"
-
+    visible_columns_lengths = filter_visible_columns(max_lengths)
+    fmt = create_table_format(visible_columns_lengths)
     ret = [
-        fmt % header,
-        "|" + "|".join(f":{'-' * n}-" for n in max_lengths) + "|",
+        fmt % filter_visible_columns(header),
+        create_table_header_separator(visible_columns_lengths),
     ]
     for r in rows:
-        ret.append(fmt % r)
+        ret.append(fmt % filter_visible_columns(r))
     return "\n".join(ret)
 
 
 class DocumentEnum:
-    kind = Enum
     cls: Enum
-    table_header = ("Symbol", "Value")
+    table_header = ("Symbol", "Value", "Domain", "Description")
 
     def __init__(self, cls: Enum) -> None:
         self.cls = cls
@@ -87,39 +223,32 @@ class DocumentEnum:
         else:
             lines.append(f"## {self.name}")
 
-        rows = [
-            (f"`{m.name}`", m.value) for m in self.cls.__members__.values()
-        ]
+        rows = []
+        for m in self.cls.__members__.values():
+            v = m.value
+            if isinstance(v, constants.VSSConstant):
+                rows.append((f"`{m.name}`", v, v.domain, v.description))
+            else:
+                rows.append((f"`{m.name}`", v, '', ''))
+
         lines.append(create_table(self.table_header, rows))
         lines.append("")
         return "\n".join(lines)
 
 
-DocumentGenerators = Union[Type[DocumentEnum]]
-Documents = Union[DocumentEnum]
-collect_map: Mapping[str, DocumentGenerators] = {
-    "Enumerations": DocumentEnum,
-}
 
-
-def collect() -> Mapping[str, Sequence[Documents]]:
-    collected = {}
+def collect() -> Sequence[DocumentEnum]:
+    collected: List[DocumentEnum] = []
     for name in constants.__all__:
         element = getattr(constants, name)
-        for key, doc in collect_map.items():
-            if issubclass(element, doc.kind):
-                collected.setdefault(key, []).append(doc(element))
+        if issubclass(element, Enum):
+            collected.append(DocumentEnum(element))
     return collected
 
 
 def get_args() -> argparse.ArgumentParser:
     ap = argparse.ArgumentParser(
         description="Generate MarkDown documentation based on code constants"
-    )
-    ap.add_argument(
-        "--kind", "-k",
-        help="If provided, restrict kind of constants to document.",
-        choices=tuple(collect_map.keys()),
     )
     ap.add_argument(
         "--name", "-n",
@@ -140,15 +269,11 @@ def main():
 
     out = args.output_file
     collected = collect()
-    for key, docs in collected.items():
-        if args.kind and args.kind != key:
+    for doc in collected:
+        if args.name and args.name != doc.name:
             continue
-        out.write(f"# {key}\n")
-        for d in docs:
-            if args.name and args.name != d.name:
-                continue
-            out.write(str(d))
-            out.write("\n")
+        out.write(str(doc))
+        out.write("\n")
 
 
 if __name__ == "__main__":
