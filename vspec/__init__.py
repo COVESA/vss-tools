d447f1ac390c7a643b29802f9bb2e55bd9be5fb5
Leonardo Ramos <oliveirarleo@gmail.com>
2021-07-14 16:37:48 -0300

    Changing undefined behavior of include_dirs
    The list(set(some_list)) can generate undefined order of list. In this
    case, the wanted behavior is to look at the current working directory
    first, and then at others.

--- vspec/__init__.py
+++ vspec/__init__.py
@@ -255,7 +255,9 @@ def load_flat_model(file_name, prefix, include_paths):
     check_yaml_usage(raw_yaml, file_name)
 
     # Recursively expand all include files.
-    expanded_includes = expand_includes(raw_yaml, prefix, list(set(include_paths + [directory])))
+    if directory not in include_paths:
+        include_paths = [directory] + include_paths
+    expanded_includes = expand_includes(raw_yaml, prefix, include_paths)
 
     # Add type: branch when type is missing.
     flat_model = cleanup_flat_entries(expanded_includes)
