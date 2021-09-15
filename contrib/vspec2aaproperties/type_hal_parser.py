5e2bcfd0d39f7ef9c88d1dc78d19dfb7985bf02a
Manu Kemppainen <manu.kemppainen@bittium.com>
2021-09-15 12:00:35 +0300

    Second draft version of the converter. - Added jinja2 usage (reused from vsc-tools) - Added CPP template (only showing items in comments currently)

--- contrib/vspec2aaproperties/type_hal_parser.py
+++ contrib/vspec2aaproperties/type_hal_parser.py
@@ -1,10 +1,12 @@
 import re
+import anytree
 def get_vehicleproperty(file):
     with open(file,"r") as f:
         content=f.read()
     propertyblock=re.search("enum VehicleProperty : int32_t {(.+?)}",content,re.DOTALL | re.MULTILINE)
     propertytypes=re.findall("^ *([A-Z_0-9]*) = \(.*?VehiclePropertyType:([A-Z_0-9]*?)$",propertyblock[1],re.DOTALL | re.MULTILINE)
-    return propertytypes
+    result={item:value for item,value in propertytypes}
+    return result
 
 if __name__ == "__main__":
     get_vehicleproperty("contrib/vspec2aaproperties/types.hal")
--- contrib/vspec2aaproperties/vspec2aaprop.py
+++ contrib/vspec2aaproperties/vspec2aaprop.py
@@ -16,18 +16,78 @@ import getopt
 from anytree import PreOrderIter
 import type_hal_parser
 import read_mapping_layer
+import jinja2
+
 
 myDir= os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(myDir, "../.."))
 import vspec
 from vspec.model.vsstree import VSSNode, VSSType
 
-def generate_from_tree(vss_tree, map_tree, type_table, output_file):
-    print("GENERATING...")
+# Set up Jinja
+jinja_env = jinja2.Environment(
+        # Use the subdirectory 'templates' relative to this file's location
+        loader =
+        jinja2.FileSystemLoader(myDir + '/../templates'),
+
+        # Templates with these extension gets automatic autoescape for HTML
+        # It's more annoying for code generation, so passing empty list for now.
+        autoescape = jinja2.select_autoescape([])
+        )
+
+# This is important. We want the control blocks in the template to NOT
+# result in any extra white space when rendering templates.
+# However, this might be a choice made by each generator, so then we need
+# to export the ability to keep these public for the using code to modify
+# them.
+jinja_env.trim_blocks = True
+jinja_env.lstrip_blocks = True
+
+default_templates = {}
+
+# Exception:
+class GeneratorError(BaseException):
+   def __init__(self, m):
+       self.msg = m
+
+def generate_from_tree(node, second=None):
+   if type(node) == list or type(node) == tuple:
+       # Generate each node and return a list of results.
+       # A list is not intended to be printed directly as output, but to be
+       # processed by a jinja filter, such as |join(', ')
+       return [generate_from_tree(x, second) for x in node]
+
+   # OK, now dispatch gen() depending on the input type
+   if second is None:          # No explicit template -> use default for the node type
+       return _gen_type(node)
+   elif type(second) == str:   # Explicit template -> use it
+       return _gen_tmpl(node, second)
+   else:
+      print(f'node is of type {type(node)}, second arg is of type {type(second)}  ({type(second).__class__}, {type(second).__class__.__name__})')
+      raise GeneratorError(f'Wrong use of gen() function! Usage: pass the node as first argument (you passed a {type(node)}), and optionally template name (str) as second argument. (You passed a {second.__name__})')
+
+# If no template is specified, use the default template for the node type.
+# A default template must be defined for this node type to use the function
+# this way.
+def _gen_type(node):
+    nodetype=type(node).__name__
+    tpl = default_templates.get(nodetype)
+    if tpl is None:
+       raise GeneratorError(f'gen() function called with node of type {nodetype} but no default template is defined for the type {nodetype}')
+    else:
+       return get_template(tpl).render({ 'item' : node})
+
+# If template name directly specified, just use it.
+def _gen_tmpl(node, templatefile: str):
+    return get_template(templatefile).render({ 'item' : node})
+
+# Get template with given name (search path should be handled by the loader)
+def get_template(filename):
+    return jinja_env.get_template(filename)
 
 def usage():
     print(
-        """Usage: vspec2aaprop.py [-I include_dir] vspec_file mapping_file types_hal_file output_file
+        """Usage: vspec2aaprop.py [-I include_dir] vspec_file mapping_file types_hal_file template_file output_file
   -I include_dir       Add include directory to search for included vspec
                        files. Can be used multiple times.
   vspec_file           The top-level vehicle specification file to parse.
@@ -53,20 +113,27 @@ if __name__ == "__main__":
         else:
             usage()
 
-    if len(args) != 4:
+    if len(args) != 5:
         usage()
 
     try:
-        tree = vspec.load_tree(args[0], include_dirs)
+        vss_tree = vspec.load_tree(args[0], include_dirs)
     except vspec.VSpecError as e:
         print("Error: {}".format(e))
         exit(255)
 
     map_tree = read_mapping_layer.load_tree(args[1])
     type_table = type_hal_parser.get_vehicleproperty(args[2])
+    #template=get_template(args[3]) #This is loaded in subroutines.
 
-    output_file = open(args[3], "w")
-    generate_from_tree(tree, map_tree, type_table, output_file)
+    jinja_env.globals.update(
+    gen=generate_from_tree,
+    vss_tree=vss_tree,
+    map_tree=map_tree,
+    type_table=type_table 
+    )
 
-    output_file.write("//DONE\n")
-    output_file.close()
+#def generate_from_tree(vss_tree, map_tree, type_table, template, output_file):
+    with open(args[4], "w") as output_file:
+        print(generate_from_tree(map_tree, args[3]),file=output_file)
+        output_file.write("//DONE\n")
