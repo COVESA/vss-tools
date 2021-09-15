89f4cb0feface2d82cf41e7021a44cdb183681ff
Manu Kemppainen <manu.kemppainen@bittium.com>
2021-09-15 15:16:00 +0300

    Added comment removal before evaluating the types.hal in type_hal_parser - Comments caused parsing to stop before the end of the table. - Updated Jinja2 template to create basic conversion (not using the scale/complex yet) - Changed type_hal_parser type_table to class to support .__getitem__() from Jinja. - getitem is used to map items like type_table['VehicleProperty::FUEL_LEVEL'] to actual types.hal table format self.table['FUEL_LEVEL']

--- contrib/vspec2aaproperties/type_hal_parser.py
+++ contrib/vspec2aaproperties/type_hal_parser.py
@@ -1,12 +1,29 @@
 import re
 import anytree
-def get_vehicleproperty(file):
-    with open(file,"r") as f:
-        content=f.read()
-    propertyblock=re.search("enum VehicleProperty : int32_t {(.+?)}",content,re.DOTALL | re.MULTILINE)
-    propertytypes=re.findall("^ *([A-Z_0-9]*) = \(.*?VehiclePropertyType:([A-Z_0-9]*?)$",propertyblock[1],re.DOTALL | re.MULTILINE)
-    result={item:value for item,value in propertytypes}
-    return result
+
+    
+class type_table:
+    def __init__(self,file):
+        with open(file,"r") as f:
+            content=f.read()
+        pattern = r"(\".*?\"|\'.*?\')|(/\*.*?\*/|//[^\r\n]*$)"
+        regex = re.compile(pattern, re.MULTILINE|re.DOTALL)
+        def _replacer(match):
+            if match.group(2) is not None:
+                return "" #remove comment
+            else: 
+                return match.group(1) #return code
+        content=regex.sub(_replacer, content)
+
+        propertyblock=re.search("enum VehicleProperty : int32_t {(.+?)}",content,re.DOTALL | re.MULTILINE)
+        propertytypes=re.findall("^ *([A-Z_0-9]*) = \(.*?VehiclePropertyType:([A-Z_0-9]*)[ \,]*$",propertyblock[1],re.DOTALL | re.MULTILINE)
+        self.table={item:value for item,value in propertytypes}
+
+    def __getitem__(self,name):
+        typestr=self.table[name.split(":")[-1]]
+        print("getitem:",name,":",typestr)
+        return typestr
+
 
 if __name__ == "__main__":
     get_vehicleproperty("contrib/vspec2aaproperties/types.hal")
