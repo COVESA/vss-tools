6be8f15399318eb14a72e02d370c6053cee54a41
Manu Kemppainen <manu.kemppainen@bittium.com>
2021-09-15 10:30:45 +0300

    First very draft converter with dependency loading Loading of vspec Loading of Android types.hal Loading of mapping yaml file. TODO: Jinja2 model and model loading TODO: Actual conversion

--- contrib/vspec2aaproperties/vspec2aaprop.py
+++ contrib/vspec2aaproperties/vspec2aaprop.py
@@ -14,22 +14,25 @@ import sys
 import os
 import getopt
 from anytree import PreOrderIter
+import type_hal_parser
+import read_mapping_layer
 
 myDir= os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(myDir, "../.."))
 import vspec
 from vspec.model.vsstree import VSSNode, VSSType
 
-def generate_from_tree(vss_tree, map_tree, output_file):
+def generate_from_tree(vss_tree, map_tree, type_table, output_file):
     print("GENERATING...")
 
 def usage():
     print(
-        """Usage: vspec2aaprop.py [-I include_dir] vspec_file output_file
+        """Usage: vspec2aaprop.py [-I include_dir] vspec_file mapping_file types_hal_file output_file
   -I include_dir       Add include directory to search for included vspec
                        files. Can be used multiple times.
   vspec_file           The top-level vehicle specification file to parse.
   mapping_file         The VSS-layer that defines mapping between VSS and AA properties
+  types_hal_file       The Android types.hal header file for VHAL type information.
   output_file          The primary file name to write C++ generated code to.)
 """
     )
@@ -50,10 +53,8 @@ if __name__ == "__main__":
         else:
             usage()
 
-    if len(args) != 3:
+    if len(args) != 4:
         usage()
-
-    output_file = open(args[1], "w")
 
     try:
         tree = vspec.load_tree(args[0], include_dirs)
@@ -62,8 +63,10 @@ if __name__ == "__main__":
         exit(255)
 
     map_tree = read_mapping_layer.load_tree(args[1])
+    type_table = type_hal_parser.get_vehicleproperty(args[2])
 
-    generate_from_tree(tree, map_tree, output_file)
+    output_file = open(args[3], "w")
+    generate_from_tree(tree, map_tree, type_table, output_file)
 
     output_file.write("//DONE\n")
     output_file.close()
