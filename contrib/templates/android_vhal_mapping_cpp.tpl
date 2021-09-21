5324bc596dc6a84b1d5059dc44abcd27a384a090
Manu Kemppainen <manu.kemppainen@bittium.com>
2021-09-21 18:56:56 +0300

    Added vspec handling to the Jinja. - Added helper wrapper class for vspec (vspec_helper) - Helper supports __getitem__() for Jinja. - vspec["Vehicle.Powertrain.FuelSystem.Level"] grabs fuel level node. - Updated the template (typo removed, vspec example in comments) - Removed debug print from Android types.hal parser - TODO: Update Jinja template to support other types. Currently supports only copy types.

--- contrib/templates/android_vhal_mapping_cpp.tpl
+++ contrib/templates/android_vhal_mapping_cpp.tpl
@@ -6,9 +6,10 @@ void AndroidVssConverter::initConversionMap(VehicleHal* vhal) {
 
 {% for key,item in map_tree.items() %}
 // VSS item: {{ key }}
+// VSS Unit: {{ vss_tree[key].data_type }}
 // AOSP item: {{ item['aospId'] }}
 // AOSP area: {{ item['aospArea'] }}
     conversionMap["{{ key }}"] = std::bind(convert{{ type_table[item['aospId']] }},
-        std::placeholders::_1, {{ item['aospId'] }}, toInt({{item['aospArea']}});
+        std::placeholders::_1, {{ item['aospId'] }}, toInt({{item['aospArea']}}));
 {% endfor %}
 }
--- contrib/vspec2aaproperties/vspec_helper.py
+++ contrib/vspec2aaproperties/vspec_helper.py
@@ -1,36 +1,9 @@
-'''
-vspec helper class to support tree[full.vspec.definition] for Jinja.
-'''
 from anytree import Resolver
-# Copyright (c) 2021 GENIVI Alliance
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at https://mozilla.org/MPL/2.0/.
-#
-# Generate code that can convert VSS signals to Android Automotive
-# vehicle properties.
-#
 
 class VSpecHelper:
-    '''
-    VSpecHelper class is a wrapper class for the vspec to enable tree[full.vspec.definition] for Jinja.
-    '''
-
-    #__init__ input the vspec tree and stores it inside the helper class.
     def __init__(self,tree):
-        '''
-        tree: vspec tree to wrap
-        usage: vss_tree = vspec_helper.VSpecHelper(vspec.load_tree(args[0], include_dirs))
-        '''
         self.tree=tree
-        #Create resolver for the anytree "name" tree.
         self.r=Resolver("name")
     def __getitem__(self,item):
-        '''
-        item: Ascii representation of the vspec item. e.g. "Vehicle.Chassis.Axle.Row2.Wheel.Left.Tire.Pressure"
-        returns vspec anytree object.
-        '''
-        #Currently vspec uses hardcoded "/" as separator while YAML defines "." separated values. Map between these for the resolver.
         return self.r.get(self.tree,f'/{item.replace(".","/")}')
 
