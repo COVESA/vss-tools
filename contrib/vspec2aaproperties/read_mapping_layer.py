d1cf3df7822a353d8b50494689746ed3d225ccd1
Manu Kemppainen <manu.kemppainen@bittium.com>
2021-09-29 07:44:05 +0300

    Added input variable handling to the complex system. Python adds ["translation"]["input"] list to the tree from yaml to achieve simple template.

--- contrib/vspec2aaproperties/read_mapping_layer.py
+++ contrib/vspec2aaproperties/read_mapping_layer.py
@@ -4,4 +4,11 @@ import yaml
 def load_tree(filepath):
     with open(filepath, "r") as f:
         tree = yaml.load(f.read(), Loader=yaml.SafeLoader)
+        # Create input items for the complex yaml formulas
+        translator=str.maketrans({"/":" ","*":" ","+":" ","(":" ",")":" ","!":" ","&":" ","%":" ","^":" ","|":" ","\t":" "})
+        for key,item in tree.items():
+            if "translation" in item:
+                if "complex" in item["translation"]:
+                    formula_elements=item["translation"]["complex"].translate(translator).split()
+                    item["translation"]["input"]=[element[1:] for element in formula_elements if element[0]=="$"]
     return tree
