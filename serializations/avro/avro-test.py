857a8f88e924a4d822e607acc1540907d597deb0
Gunnar Andersson <gandersson@covesa.global>
2021-10-27 16:13:23 +0200

    avro: rewrite with fastavro and internal schemas
    1) Using fastavro library instead of standard python.avro.
    2) Schema(s) now defined in python (inside source file for now) instead
       of in JSON.

--- serializations/avro/avro-test.py
+++ serializations/avro/avro-test.py
@@ -1,48 +1,120 @@
 #!/usr/bin/env python3
 
-import avro.schema
+import os
+import fastavro
 import sys
 from avro.datafile import DataFileReader, DataFileWriter
 from avro.io import DatumReader, DatumWriter
 
-if len(sys.argv) < 2:
-#    print(f"usage: #{sys.argv[0]} <schemafile.avsc>")
-#    sys.exit(1)
-    schemafile = "TimeStampedRecord.avsc"
-    print(f"Using default schema: {schemafile}")
-else:
-    schemafile = sys.argv[1]
+# Schemas are here defined recursively, starting with the
+# innermost fundamental types, and then building up more complex types from
+# them.
 
-schema = avro.schema.parse(open(schemafile, "rb").read())
+# While doing so, "schemadict" is used by fastavro to remember the
+# previously parsed Types, so that they can be referred to by name in the
+# subsequent schemas.
 
-# Test output a sequence of timeseries records
+schemadict = {}
 
-#{
-#  "type": "record",
-#  "name": "TimeStampedRecord",
-#  "fields" : [
-#         { "name" : "ts", "type" : "string" },
-#         { "name" : "value", "type" : [ "int", "long", "float", "double", "string", "boolean" ] }
-#  ]
-#}
+# Next one specifies that a value can be either a single simple type (
+# one of several choices), or an Array of such items.  Value specifies all
+# possible encodings of a single VSS value.
+s = {
+   "type" : "record",
+   "name" : "Value",
+   "fields" : [
+      { "name" : "item", "type" : [
+            # VSS values are either one of a choice of simple values:
+            # (simplified down to available AVRO types here)
+            "int", "long", "float", "double", "string", "boolean",
+            # ... or a signal value can be an array of values.
+            { "type" : "array",
+              "items" : [ "int", "long", "float", "double", "string", "boolean" ]
+            }
+         ]
+      }
+   ]
+}
+fastavro.parse_schema(s, schemadict)
+
+# A Time Stamped Record keeps a single VSS signal value, and a time stamp
+# In this case we use a long integer to encode time stamps.
+s = {
+   "type" : "record",
+   "name" : "TimeStampedRecord",
+   "fields" : [
+      { "name" : "ts", "type" : "long" },
+      { "name" : "value", "type" : "Value" }
+   ]
+}
+
+fastavro.parse_schema(s, schemadict)
+
+# A Time Series is a definition of a single signal name
+# and then a sequence of values with time stams
+s = {
+   "type" : "record",
+   "name" : "TimeSeries",
+   "fields" : [
+      { "name" : "signal_identifier", "type": "string"},
+      { "name" : "count", "type" : "int" },
+      { "name" : "items", "type" :  { "type" : "array", "items" : "TimeStampedRecord" } }
+   ]
+}
+
+# Final parsing defines the TimeSeries schema
+schema_TimeSeries = fastavro.parse_schema(s, schemadict)
+
+# OK, schema now contains the full schema definition
 
 # Encode data into file
+# (Note this follows (only) the TimeSeries schema)
 
-# (Note this follows (only) the TimeStampedRecord schema)
+# Example time series values
+vals = [
+        { "signal_identifier" : "Vehicle.Cabin.Temperature",
+          "count" : 4,
+          "items" : [
+             { "ts" : 0, "value" : { "item" : -11 } },
+             { "ts" : 1, "value" : { "item" : 2 } },
+             { "ts" : 2, "value" : { "item" : -21 } },
+             { "ts" : 3, "value" : { "item" : 5 } },
+             { "ts" : 4, "value" : { "item" : 2 } },
+             { "ts" : 5, "value" : { "item" : 230 } },
+             { "ts" : 6, "value" : { "item" : 2 } },
+             { "ts" : 7, "value" : { "item" : 7 } },
+             { "ts" : 8, "value" : { "item" : 3811 } },
+             { "ts" : 9, "value" : { "item" : 2 } },
+             { "ts" : 10, "value" : { "item" : 2 } },
+             { "ts" : 11, "value" : { "item" : -14 } },
+             { "ts" : 12, "value" : { "item" : 2 } },
+             { "ts" : 13, "value" : { "item" : -147 } },
+             { "ts" : 14, "value" : { "item" : 0 } }
+      ]
+  }
+]
 
-writer = DataFileWriter(open("testoutput.avro", "wb"), DatumWriter(), schema)
-writer.append({ "ts" : "20210826123100.45Z", "value" : int(2304) })
-writer.append({ "ts" : "20210826123101.35Z", "value" : int(2314) })
-writer.append({ "ts" : "20210826123102.41Z", "value" : int(2388) })
-writer.append({ "ts" : "20210826123103.50Z", "value" : int(2481) })
-writer.append({ "ts" : "20210826123104.39Z", "value" : int(2500) })
-writer.append({ "ts" : "20210826123105.44Z", "value" : int(2533) })
-#writer.append({"values": "Vehicle.Powertrain.CombustionEngine.Engine.Speed", "count": 4})
-writer.close()
+# Example with an array-typed signal (array of strings)
+vals2 = [
+        { "signal_identifier" : "Vehicle.Cabin.Temperature",
+            "count" : 4,
+            "items" : [
+                { "ts" : 0, "value" : { "item" : [  "one" , "two" , "three" ] } },
+                { "ts" : 1, "value" : { "item" : [ "another" , "value" , "sequence", "of", "stringvalues" ] } },
+                { "ts" : 2, "value" : { "item" : [ "foo" , "bar" , "baz" ] } },
+                ]
+            }
+]
+
+# Write data to avro encoded (binary) file
+fn = 'testoutput.avro'
+os.remove(fn)
+with open(fn, 'wb') as fout:
+     fastavro.writer(fout, schema_TimeSeries , vals + vals2)
 
 # Read back the file, decode and print the data
-reader = DataFileReader(open("testoutput.avro", "rb"), DatumReader())
-for user in reader:
-    print(user)
-reader.close()
+with open('testoutput.avro', 'rb') as fin:
+   avro_reader = fastavro.reader(fin, schema_TimeSeries)
+   for record in avro_reader:
+       print("Read record:" + str(record))
 
