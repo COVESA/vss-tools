d8bc72dec706c5245dde2b76cae3c05e5bf36029
Daniel Wilms <Daniel.DW.Wilms@bmw.de>
2022-04-27 18:35:10 +0300

    Moved vspec2franca to anytree
    - changed to anytree in creation
    - adopted struct and generated output
    
    @gunnarx, could you please check, if correct?

--- vspec2franca.py
+++ vspec2franca.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#
+# (c) 2022 BMW Group
 # (C) 2016 Jaguar Land Rover
 #
 # All files and artifacts in this repository are licensed under the
@@ -11,122 +11,7 @@
 #
 
 import sys
-import vspec
-import argparse
-
-
-def traverse_tree(tree, outf, prefix_arr, is_first_element):
-    # Convert a prefix array of path elements to a string
-    def prefix_to_string(prefix_arr):
-        if len(prefix_arr) == 0:
-            return ""
-
-        res = prefix_arr[0]
-        for elem in prefix_arr[1:]:
-            res = "{}.{}".format(res, elem)
-
-        return res
-
-
-    # Traverse all elemnts in tree.
-    for key, val in tree.items():
-        # Is this a branch?
-        if "children" in val:
-            # Yes. Recurse
-            traverse_tree(val['children'], outf, prefix_arr + [ key ], is_first_element)
-            continue
-
-        # Drop a comma in before the next element.
-        if not is_first_element:
-            outf.write(",\n")
-
-        outf.write("""{{
-    name: "{}"
-    type: "{}"
-    description: "{}"
-""".format(prefix_to_string(prefix_arr + [ key ]),
-           val['type'],
-           val['description']))
-
-        if "datatype" in val:
-            outf.write("    datatype: {}\n".format(val["datatype"]))
-
-        if "uuid" in val:
-            outf.write("    uuid: {}\n".format(val["uuid"]))
-
-        if "min" in val:
-            outf.write("    min: {}\n".format(val["min"]))
-
-        if "max" in val:
-            outf.write("    max: {}\n".format(val["max"]))
-
-        if "unit" in val:
-            outf.write("    unit: {}\n".format(val["unit"]))
-
-        if "allowed" in val:
-            outf.write("    allowed: {}\n".format(val["allowed"]))
-
-        if "sensor" in val:
-            outf.write("    sensor: {}\n".format(val["sensor"]))
-
-        if "actuator" in val:
-            outf.write("    actuator: {}\n".format(val["actuator"]))
-
-        outf.write("}\n")
-        is_first_element = False
-
+import vspec2x
 
 if __name__ == "__main__":
-    # The arguments we accept
-
-    parser = argparse.ArgumentParser(description='Convert vspec to franca.')
-    parser.add_argument('-I', '--include-dir', action='append',  metavar='dir', type=str, default=[],
-                    help='Add include directory to search for included vspec files.')
-    parser.add_argument('-s', '--strict', action='store_true', help='Use strict checking: Terminate when non-core attribute is found.' )
-    parser.add_argument('-v', '--vssversion', type=str, default="unspecified version",  help='VSS version' )
-    parser.add_argument('vspec_file', metavar='<vspec_file>', help='The vehicle specification file to convert.')
-    parser.add_argument('franca_file', metavar='<franca file>', help='The file to output the Franca IDL spec to.')
-
-    args = parser.parse_args()
-
-    strict=args.strict
-    
-    include_dirs = ["."]
-    include_dirs.extend(args.include_dir)
-
-    franca_out = open(args.franca_file, "w")
-
-    
-    try:
-        tree = vspec.load(args.vspec_file, include_dirs)
-    except vspec.VSpecError as e:
-        print("Error: {}".format(e))
-        sys.exit(255)
-
-    franca_out.write(
-"""// Copyright (C) 2016, GENIVI Alliance
-//
-// This program is licensed under the terms and conditions of the
-// Mozilla Public License, version 2.0.  The full text of the
-// Mozilla Public License is at https://www.mozilla.org/MPL/2.0/
-
-const UTF8String VSS_VERSION = "{}"
-
-struct SignalSpec {{
-    UInt32 id
-    String name
-    String type
-    String description
-}}
-
-const SignalSpec[] signal_spec = [
-""".format(args.vssversion))
-
-    traverse_tree(tree, franca_out, [], True)
-
-    franca_out.write(
-"""
-]
-""")
-
-    franca_out.close()
+    vspec2x.main(["--format", "franca"]+sys.argv[1:])
---
+++ vssexporters/vss2franca.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+# (c) 2021 BMW Group
+#
+# All files and artifacts in this repository are licensed under the
+# provisions of the license provided by the LICENSE file in this repository.
+#
+#
+# Convert vspec tree to franca
+
+
+
+import argparse
+from vspec.model.vsstree import VSSNode, VSSType
+from anytree import PreOrderIter
+    
+def add_arguments(parser: argparse.ArgumentParser):
+   #no additional output for Franca at this moment
+   parser.description="The Franca exporter does not currently support the no-uuid option."
+   parser.add_argument('-v', metavar='version', help=" Add version information to franca file.")
+
+#Write the header line
+def print_franca_header(file, version="unknown"):
+    file.write(f"""
+// Copyright (C) 2022, COVESA
+//
+// This program is licensed under the terms and conditions of the
+// Mozilla Public License, version 2.0.  The full text of the
+// Mozilla Public License is at https://www.mozilla.org/MPL/2.0/
+
+const UTF8String VSS_VERSION = "{version}"
+
+struct SignalSpec {{
+    UInt32 id
+    String name
+    String type
+    String description
+    String datatype
+    String unit
+    Double min
+    Double max
+}}
+
+const SignalSpec[] signal_spec = [
+""")
+
+
+#Write the data lines
+def print_franca_content(file, tree):
+    output = ""
+    for tree_node in PreOrderIter(tree):
+        if tree_node.parent:
+            if output:
+                output += ",\n{"
+            else:
+                output += "{"
+            output += f"\tname: \"{tree_node.qualified_name('.')}\""
+            output += f",\n\ttype: \"{tree_node.type.value}\""
+            output += f",\n\tdescription: \"{tree_node.description}\""
+            if tree_node.has_data_type():
+                output += f",\n\tdatatype: \"{tree_node.data_type.value}\""
+            output += f",\n\tuuid: \"{tree_node.uuid}\""
+            if tree_node.has_unit():
+                output += f",\n\tunit: \"{tree_node.unit.value}\""
+            if tree_node.min:
+                output += f",\n\tmin: {tree_node.min}"
+            if tree_node.max:
+                output += f",\n\tmax: {tree_node.max}"
+            if tree_node.allowed:
+                output += f",\n\tallowed: {tree_node.allowed}"
+            
+            output += "\n}"
+    file.write(f"{output}")
+
+def export(config: argparse.Namespace, root: VSSNode):
+    print("Generating Franca output...")
+    outfile=open(config.output_file,'w')
+    print_franca_header(outfile, config.v)
+    print_franca_content(outfile, root)
+    outfile.write("\n]")
+    outfile.close()
