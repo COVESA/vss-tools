eda05496b9cab87a5c8dbee529838c891fefc54f
Daniel Wilms <Daniel.DW.Wilms@bmw.de>
2022-05-26 13:25:22 +0300

    Upgrade of graphql as community supported tool
    - adopt graphql to use exporters
    - remove additional fields `source` and `channel`
    - add graphql specific parameter to add additional metadata
    - adopt readme
    - remove graphql exporters from contrib
    
    call with:
    ```
    ${TOOLSDIR}/vspec2graphql.py -I ./spec --gqlfield "source" "Source System" --gqlfield "channel" "Collecting channel" ./spec/VehicleSignalSpecification.vspec vss_rel_$$(cat VERSION).graphql.ts
    ```
    
    Signed-off-by: Daniel Wilms <Daniel.DW.Wilms@bmw.de>

--- docs/VSS2GRAPHQL.md
+++ docs/VSS2GRAPHQL.md
@@ -1,6 +1,6 @@
-## What does vspec2graphql do?
+## vspec2graphql
 
-This allows to automatically generate a graphql schema that can represent VSS data.
+This exporter allows to automatically generate a graphql schema that can represent VSS data.
 The resulting schema does only allow querying information. Mutations are not supported.
 
 The resulting schema will look something like this:
@@ -37,23 +37,36 @@ type Vehicle_VehicleIdentification_Brand {
   """Value: Vehicle brand or manufacturer"""
   value: String
 
-  """Source system: Vehicle brand or manufacturer"""
-  source: String
-
-  """Collecting channel: Vehicle brand or manufacturer"""
-  channel: String
-
   """Timestamp: Vehicle brand or manufacturer"""
   timestamp: String
 }
 ```
 
-Above, `source` and `channel` are free text fields which can be used to include information on the origin of the data. Content is implementation specific. `timestamp` shall be the timestamp of the observation formatted according to ISO 8601 with UTC time zone.
-
 Every leaf has a timestamp. This is supposed to contain the date of the last modification of the value. 
 Queries can then filter data that has been recorded after a given timestamp.
 
-After adjusting the vspec path in the makefile you should be able to execute 
-`make graphql` in `vss-tools/vspec2graphql`.
-The generated schema can then be found in: `vss-tools/vspec2graphql/generated/result.graphql`
+### Additional leaf parameters
 
+As for `timestamp` in some scenarios it makes sense to add certain metadata like the `source` of a
+served signal or additional privacy information. Therefore the tool has an additional calling parameter
+`--gqlfield <name> <description>`, which takes the name and description of the additional field, like:
+
+```
+--gqlfield "source" "Source System"
+```
+
+Resulting in the following leaf in the schema:
+
+```
+"""Vehicle brand or manufacturer"""
+type Vehicle_VehicleIdentification_Brand {
+  """Value: Vehicle brand or manufacturer."""
+  value: String
+
+  """Timestamp: Vehicle brand or manufacturer."""
+  timestamp: String
+
+  """ Source System: Vehicle brand or manufacturer."""
+  source: String
+}
+```
--- vssexporters/vss2graphql.py
+++ vssexporters/vss2graphql.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 #
 # Copyright (C) 2022, Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
 #
@@ -5,10 +7,11 @@
 # provisions of the license provided by the LICENSE file in this repository.
 #
 
-import re
+import argparse
 from anytree import RenderTree, PreOrderIter
 from vspec.model.vsstree import VSSNode, camel_back
 from vspec.model.constants import VSSDataType
+from vspec import VSpecError
 from anytree import PostOrderIter
 from typing import Dict
 
@@ -56,8 +59,14 @@ GRAPHQL_TYPE_MAPPING = {
     VSSDataType.STRING_ARRAY: GraphQLList(GraphQLString),
 }
 
-def get_schema_from_tree(root_node: VSSNode) -> str:
-    """Takes a VSSNode. Returns a graphql schema as string."""
+def add_arguments(parser: argparse.ArgumentParser):
+   #no additional output for graphql at this moment
+   parser.description="The graphql exporter does not currently support the no-uuid option."
+   parser.add_argument('--gqlfield', action='append', nargs=2, help=" Add additional fields to the nodes in the graphql schema. use: <field_name> <description>")
+
+
+def get_schema_from_tree(root_node: VSSNode, additional_leaf_fields: list) -> str:
+    """Takes a VSSNode and additional fields for the leafs. Returns a graphql schema as string."""
     args = dict(
         id=GraphQLArgument(
             GraphQLNonNull(GraphQLString),
@@ -74,35 +83,39 @@ def get_schema_from_tree(root_node: VSSNode) -> str:
 
     root_query = GraphQLObjectType(
         "Query",
-        lambda: {"vehicle": GraphQLField(to_gql_type(root_node), args)},
+        lambda: {"vehicle": GraphQLField(to_gql_type(root_node, additional_leaf_fields), args)},
     )
     return print_schema(GraphQLSchema(root_query))
 
 
-def to_gql_type(node: VSSNode) -> GraphQLObjectType:
+def to_gql_type(node: VSSNode, additional_leaf_fields: list) -> GraphQLObjectType:
     return GraphQLObjectType(
         name=node.qualified_name("_"),
-        fields=leaf_fields(node) if hasattr(node, "data_type") else branch_fields(node),
+        fields=leaf_fields(node, additional_leaf_fields) if hasattr(node, "data_type") else branch_fields(node,additional_leaf_fields),
         description=node.description,
     )
 
 
-def leaf_fields(node: VSSNode) -> Dict[str, GraphQLField]:
+def leaf_fields(node: VSSNode, additional_leaf_fields: list) -> Dict[str, GraphQLField]:
     field_dict = {
         "value": field(node, "Value: ", GRAPHQL_TYPE_MAPPING[node.data_type]),
-        "source": field(node, "Source system: "),
-        "channel": field(node, "Collecting channel: "),
-        "timestamp": field(node, "Timestamp: "),
+        "timestamp": field(node, "Timestamp: ")
     }
+    if additional_leaf_fields:
+        for additional_field in additional_leaf_fields:
+            if len(additional_field) == 2:
+                field_dict[additional_field[0]] = field(node, f" {str(additional_field[1])}: ")
+            else:
+                raise VSpecError("","","Incorrect format of graphql field specification")
     if hasattr(node, "unit"):
         field_dict["unit"] = field(node, "Unit of ")
     return field_dict
 
 
-def branch_fields(node: VSSNode) -> Dict[str, GraphQLField]:
+def branch_fields(node: VSSNode, additional_leaf_fields: list) -> Dict[str, GraphQLField]:
     # we only consider nodes that either have children or are leave with a data_type
     valid = (c for c in node.children if len(c.children) or hasattr(c, "data_type"))
-    return {camel_back(c.name): field(c, type=to_gql_type(c)) for c in valid}
+    return {camel_back(c.name): field(c, type=to_gql_type(c, additional_leaf_fields)) for c in valid}
 
 
 def field(node: VSSNode, description_prefix="", type=GraphQLString) -> GraphQLField:
@@ -111,3 +124,11 @@ def field(node: VSSNode, description_prefix="", type=GraphQLString) -> GraphQLFi
         deprecation_reason=node.deprecation or None,
         description=f"{description_prefix}{node.description}",
     )
+
+
+def export(config: argparse.Namespace, root: VSSNode):
+    print("Generating graphql output...")
+    outfile=open(config.output_file,'w')
+    outfile.write(get_schema_from_tree(root, config.gqlfield))
+    outfile.write("\n")
+    outfile.close()
