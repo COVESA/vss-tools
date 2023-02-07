53f20a497bbb756279678eac9bf5ced388030414
Krishna Koppolu <kkoppolu@amazon.com>
2023-02-07 01:02:11 +0000

    Topic: CommandLine
    Fix copyright statement

--- vspec/loggingconfig.py
+++ vspec/loggingconfig.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-# (c) 2022 BMW Group
-# (c) 2022 Robert Bosch GmbH
-# (c) 2016 Jaguar Land Rover
-#
 # All files and artifacts in this repository are licensed under the
 # provisions of the license provided by the LICENSE file in this repository.
 #
