2487af96c0e267e60982add8289fb94365d7d6c7
Erik Jaegervall <erik.jaegervall@se.bosch.com>
2023-02-28 13:23:26 +0100

    Make no UUID default
    Also adapt pre-hooks to ignore test files used for comparison
    
    Signed-off-by: Erik Jaegervall <erik.jaegervall@se.bosch.com>

--- tests/vspec/test_types_with_uuid/test_uuid.py
+++ tests/vspec/test_types_with_uuid/test_uuid.py
@@ -17,18 +17,17 @@ def change_test_dir(request, monkeypatch):
     monkeypatch.chdir(request.fspath.dirname)
 
 
-def run_exporter(exporter, argument):
-    test_str = "../../../vspec2" + exporter + ".py -u ../test_units.yaml " + argument + \
-        " test.vspec out." + exporter + " 1> out.txt 2>&1"
+def run_exporter(exporter, argument, compare_suffix):
+    test_str = "../../../vspec2" + exporter + ".py " + argument + \
+        " -u ../test_units.yaml test.vspec out." + exporter + " 1> out.txt 2>&1"
     result = os.system(test_str)
     assert os.WIFEXITED(result)
     assert os.WEXITSTATUS(result) == 0
-    test_str = "diff out." + exporter + " expected." + exporter
+    test_str = "diff out." + exporter + " expected_" + compare_suffix + "." + exporter
     result = os.system(test_str)
     os.system("rm -f out." + exporter + " out.txt")
     assert os.WIFEXITED(result)
     assert os.WEXITSTATUS(result) == 0
-
 
 def test_uuid(change_test_dir):
 
@@ -37,48 +36,32 @@ def test_uuid(change_test_dir):
     # target
     exporters = ["json", "ddsidl", "csv", "yaml", "franca", "graphql"]
     for exporter in exporters:
-        run_exporter(exporter, "--uuid")
+        run_exporter(exporter, "--uuid", "uuid")
+        run_exporter(exporter, "--no-uuid", "no_uuid")
         # Same behavior expected if no argument
-        run_exporter(exporter, "")
+        run_exporter(exporter, "", "no_uuid")
 
 
-def test_warning_no_parameter(change_test_dir):
-    test_str = "../../../vspec2json.py -u ../test_units.yaml test.vspec out.json 1> out.txt 2>&1"
+def run_deprecation_test(argument, deprecation_expected: bool):
+    test_str = "../../../vspec2json.py " + argument + " -u ../test_units.yaml test.vspec out.json 1> out.txt 2>&1"
     result = os.system(test_str)
     assert os.WIFEXITED(result)
     assert os.WEXITSTATUS(result) == 0
-    test_str = 'grep \"From VSS 4.0 the default behavior for printing uuid will change\" out.txt > /dev/null'
+    test_str = 'grep \"The argument --no-uuid is deprecated and will be removed in VSS 5.0\" out.txt > /dev/null'
     result = os.system(test_str)
     os.system("cat out.txt")
     os.system("rm -f out.json out.txt")
     assert os.WIFEXITED(result)
-    assert os.WEXITSTATUS(result) == 0
+    if deprecation_expected:
+        assert os.WEXITSTATUS(result) == 0
+    else:
+        assert os.WEXITSTATUS(result) != 0
 
 
-def test_no_warning_uuid_parameter(change_test_dir):
-    test_str = "../../../vspec2json.py -u ../test_units.yaml --uuid test.vspec out.json > out.txt"
-    result = os.system(test_str)
-    assert os.WIFEXITED(result)
-    assert os.WEXITSTATUS(result) == 0
-    test_str = 'grep \"From VSS 4.0 the default behavior for printing uuid will change\" out.txt > /dev/null'
-    result = os.system(test_str)
-    os.system("cat out.txt")
-    os.system("rm -f out.json out.txt")
-    assert os.WIFEXITED(result)
-    assert os.WEXITSTATUS(result) != 0
-
-
-def test_no_warning_no_uuid_parameter(change_test_dir):
-    test_str = "../../../vspec2json.py -u ../test_units.yaml --no-uuid test.vspec out.json 1> out.txt 2>&1"
-    result = os.system(test_str)
-    assert os.WIFEXITED(result)
-    assert os.WEXITSTATUS(result) == 0
-    test_str = 'grep \"From VSS 4.0 the default behavior for printing uuid will change\" out.txt > /dev/null'
-    result = os.system(test_str)
-    os.system("cat out.txt")
-    os.system("rm -f out.json out.txt")
-    assert os.WIFEXITED(result)
-    assert os.WEXITSTATUS(result) != 0
+def test_deprecation(change_test_dir):
+    run_deprecation_test("", False)
+    run_deprecation_test("--uuid", False)
+    run_deprecation_test("--no-uuid", True)
 
 
 def test_error_no_uuid_uuid_parameter(change_test_dir):
