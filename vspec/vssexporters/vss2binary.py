4161ac476d42a37a63b1f7324af62bb010918a20
Erik Jaegervall <erik.jaegervall@se.bosch.com>
2023-05-04 09:45:44 +0200

    Preparing for PyPI release
    Signed-off-by: Erik Jaegervall <erik.jaegervall@se.bosch.com>

--- vspec/vssexporters/vss2binary.py
+++ vspec/vssexporters/vss2binary.py
@@ -9,16 +9,21 @@
 # Convert vspec tree to binary format
 
 import argparse
+import logging
 import ctypes
 import os.path
 from vspec.model.vsstree import VSSNode, VSSType
 
-out_file=""
-_cbinary=None
+out_file = ""
+_cbinary = None
 
-def createBinaryCnode(fname, nodename, nodetype, uuid, description, nodedatatype, nodemin, nodemax, unit, allowed, defaultAllowed, validate, children):
+
+def createBinaryCnode(fname, nodename, nodetype, uuid, description, nodedatatype, nodemin, nodemax, unit, allowed,
+                      defaultAllowed, validate, children):
     global _cbinary
-    _cbinary.createBinaryCnode(fname, nodename, nodetype, uuid, description, nodedatatype, nodemin, nodemax, unit, allowed, defaultAllowed, validate, children)
+    _cbinary.createBinaryCnode(fname, nodename, nodetype, uuid, description, nodedatatype, nodemin, nodemax, unit,
+                               allowed, defaultAllowed, validate, children)
+
 
 def allowedString(allowedList):
     allowedStr = ""
@@ -27,11 +32,13 @@ def allowedString(allowedList):
 #    print("allowedstr=" + allowedStr + "\n")
     return allowedStr
 
+
 def hexAllowedLen(allowed):
     hexDigit1 = len(allowed) // 16
     hexDigit2 = len(allowed) - hexDigit1*16
 #    print("Hexdigs:" + str(hexDigit1) + str(hexDigit2))
     return "".join([intToHexChar(hexDigit1), intToHexChar(hexDigit2)])
+
 
 def intToHexChar(hexInt):
     if (hexInt < 10):
@@ -39,8 +46,9 @@ def intToHexChar(hexInt):
     else:
         return chr(hexInt - 10 + ord('A'))
 
+
 def add_arguments(parser: argparse.ArgumentParser):
-    parser.description="The binary exporter does not support any additional arguments."
+    parser.description = "The binary exporter does not support any additional arguments."
 
 
 def export_node(node, generate_uuid, out_file):
@@ -61,12 +69,8 @@ def export_node(node, generate_uuid, out_file):
     nodeunit = ""
     nodeallowed = ""
     nodedefault = ""
-    nodecomment = ""
     nodeuuid = ""
-    nodevalidate = "" #exported to binary
-    # not exported to binary
-    nodedeprecation = ""
-    nodeaggregate = ""
+    nodevalidate = ""  # exported to binary
 
     if node.type == VSSType.SENSOR or node.type == VSSType.ACTUATOR or node.type == VSSType.ATTRIBUTE:
         nodedatatype = str(node.datatype.value)
@@ -89,26 +93,12 @@ def export_node(node, generate_uuid, out_file):
         nodedefault = str(node.default)
     b_nodedefault = nodedefault.encode('utf-8')
 
-    if node.deprecation != "":
-        nodedeprecation = node.deprecation
-    b_nodedeprecation = nodedeprecation.encode('utf-8')
-
     # in case of unit or aggregate, the attribute will be missing
     try:
         nodeunit = str(node.unit.value)
     except AttributeError:
         pass
     b_nodeunit = nodeunit.encode('utf-8')
-
-    try:
-        nodeaggregate = node.aggregate
-    except AttributeError:
-        pass
-    b_nodeaggregate = nodeaggregate.encode('utf-8')
-
-    if node.comment != "":
-        nodecomment = node.comment
-    b_nodecomment = nodecomment.encode('utf-8')
 
     if generate_uuid:
         nodeuuid = node.uuid
@@ -120,7 +110,8 @@ def export_node(node, generate_uuid, out_file):
 
     b_fname = out_file.encode('utf-8')
 
-    createBinaryCnode(b_fname, b_nodename, b_nodetype, b_nodeuuid, b_nodedescription, b_nodedatatype, b_nodemin, b_nodemax, b_nodeunit, b_nodeallowed, b_nodedefault, b_nodevalidate, children)
+    createBinaryCnode(b_fname, b_nodename, b_nodetype, b_nodeuuid, b_nodedescription, b_nodedatatype, b_nodemin,
+                      b_nodemax, b_nodeunit, b_nodeallowed, b_nodedefault, b_nodevalidate, children)
 
     for child in node.children:
         export_node(child, generate_uuid, out_file)
@@ -130,18 +121,19 @@ def export(config: argparse.Namespace, root: VSSNode, print_uuid):
     global _cbinary
     dllName = "../../binary/binarytool.so"
     dllAbsPath = os.path.dirname(os.path.abspath(__file__)) + os.path.sep + dllName
+    if not os.path.isfile(dllAbsPath):
+        logging.error("The required library binarytool.so is not available, exiting!")
+        logging.info("You must build the library, "
+                     "see https://github.com/COVESA/vss-tools/blob/master/binary/README.md!")
+        return
     _cbinary = ctypes.CDLL(dllAbsPath)
 
-    #void createBinaryCnode(char* fname, char* name, char* type, char* uuid, char* descr, char* datatype, char* min, char* max, char* unit, char* allowed, char* defaultAllowed, char* validate, int children);
-    _cbinary.createBinaryCnode.argtypes = (ctypes.c_char_p,ctypes.c_char_p,ctypes.c_char_p,ctypes.c_char_p,ctypes.c_char_p,ctypes.c_char_p,ctypes.c_char_p,ctypes.c_char_p,ctypes.c_char_p,ctypes.c_char_p,ctypes.c_char_p,ctypes.c_char_p,ctypes.c_int)
-    
-    print("Generating binary output...")
+    _cbinary.createBinaryCnode.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p,
+                                           ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p,
+                                           ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p,
+                                           ctypes.c_int)
+
+    logging.info("Generating binary output...")
     out_file = config.output_file
     export_node(root, print_uuid, out_file)
-    print("Binary output generated in " + out_file)
-
-
-
-
-
-
+    logging.info("Binary output generated in " + out_file)
