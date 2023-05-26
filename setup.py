34b932456128fa6e3de8ee68a90764b58461c027
Erik Jaegervall <erik.jaegervall@se.bosch.com>
2023-05-26 07:55:57 +0200

    Step pypi version to release version without printout
    Signed-off-by: Erik Jaegervall <erik.jaegervall@se.bosch.com>

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     description='COVESA Vehicle Signal Specification tooling.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='4.0',
+    version='4.0.1',
     url='https://github.com/COVESA/vss-tools',
     license='Mozilla Public License 2.0',
     packages=find_packages(exclude=('tests', 'contrib')),
