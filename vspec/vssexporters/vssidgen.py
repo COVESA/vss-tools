19cd1e9b755f062745c6edaa75d055d456a1e44b
Adnan Bekan <Adnan.Bekan@bmwgroup.com>
2023-10-05 09:23:51 +0200

    Update header.
    Signed-off-by: Adnan Bekan <Adnan.Bekan@bmwgroup.com>

--- vspec/vssexporters/vssidgen.py
+++ vspec/vssexporters/vssidgen.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
-
-
+#
+# Copyright (c) 2023 Contributors to COVESA
+#
+# This program and the accompanying materials are made available under the
+# terms of the Mozilla Public License 2.0 which is available at
+# https://www.mozilla.org/en-US/MPL/2.0/
+#
+# SPDX-License-Identifier: MPL-2.0
+#
 # Generate IDs of 4bytes size, 3 bytes incremental value + 1 byte for layer id.
 
 from vspec.model.vsstree import VSSNode
