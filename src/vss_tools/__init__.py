7340d4c0c7bddb559d105689c75be8d0d2ad9dec
Sebastian Schleemilch <sebastian.schleemilch@bmw.de>
2024-06-24 14:34:56 +0200

    Added missing header
    Signed-off-by: Sebastian Schleemilch <sebastian.schleemilch@bmw.de>

---
+++ src/vss_tools/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2021 Contributors to COVESA
+#
+# This program and the accompanying materials are made available under the
+# terms of the Mozilla Public License 2.0 which is available at
+# https://www.mozilla.org/en-US/MPL/2.0/
+#
+# SPDX-License-Identifier: MPL-2.0
