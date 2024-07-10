8ba82fca431540428dbeb6a80cc85bd2d17f6697
Niclas Wesemann <niclas.wesemann@motius.de>
2024-07-10 10:16:53 +0200

    undo making staticUID a core attribute
    Signed-off-by: Niclas Wesemann <niclas.wesemann@motius.de>

--- src/vss_tools/vspec/model/vsstree.py
+++ src/vss_tools/vspec/model/vsstree.py
@@ -68,7 +68,6 @@ class VSSNode(Node):
         "arraysize",
         "comment",
         "$file_name$",
-        "staticUID",
         "fka",
         "delete",
         "constUID",
@@ -95,7 +94,6 @@ class VSSNode(Node):
     deprecation = ""
     fka = ""
     delete: bool = False
-    staticUID: str | None = None
     constUID: str | None = None
 
     # Reference nodes for using definitions from previously existing
