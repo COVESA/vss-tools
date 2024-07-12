39f713a357d61c979588dd21574eacf582a7f72a
Sebastian Schleemilch <sebastian.schleemilch@bmw.de>
2024-07-12 16:22:45 +0200

    CI --help test + headers
    Signed-off-by: Sebastian Schleemilch <sebastian.schleemilch@bmw.de>

--- src/vss_tools/vspec/cli_options.py
+++ src/vss_tools/vspec/cli_options.py
@@ -1,3 +1,11 @@
+# Copyright (c) 2023 Contributors to COVESA
+#
+# This program and the accompanying materials are made available under the
+# terms of the Mozilla Public License 2.0 which is available at
+# https://www.mozilla.org/en-US/MPL/2.0/
+#
+# SPDX-License-Identifier: MPL-2.0
+
 import rich_click as click
 from rich_click import option
 from pathlib import Path
