72f7bea34b74fda1a337587cba93fcfc634179db
Erik Jaegervall <erik.jaegervall@se.bosch.com>
2024-07-15 16:12:41 +0200

    Test that ddsidl is correct
    Signed-off-by: Erik Jaegervall <erik.jaegervall@se.bosch.com>

---
+++ tests/ddsidl/test_ddsidl.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2024 Contributors to COVESA
+#
+# This program and the accompanying materials are made available under the
+# terms of the Mozilla Public License 2.0 which is available at
+# https://www.mozilla.org/en-US/MPL/2.0/
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from pathlib import Path
+import subprocess
+
+HERE = Path(__file__).resolve().parent
+TEST_UNITS = HERE / ".." / "vspec" / "test_units.yaml"
+
+
+def test_ddsldl(tmp_path):
+    """
+    Tests that generated ddsidl is accepted by idlc
+    """
+
+    cmd = f"vspec2ddsidl -u {TEST_UNITS} {HERE / 'test.vspec'} test.idl"
+    subprocess.run(cmd.split(), check=True)
+    cmd = "idlc -l py test.idl"
+    subprocess.run(cmd.split(), check=True)
+
+    # Basic sanity check that output is as expected
+    cmd = "grep -i A.String A/_test.py"
+    subprocess.run(cmd.split(), check=True)
