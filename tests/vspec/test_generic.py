5c40468973758402d765bc6d4aaa02545ce808ac
Niclas Wesemann <niclas.wesemann@motius.de>
2024-07-18 14:13:33 +0200

    parametrize allowed and generic tests
    Signed-off-by: Niclas Wesemann <niclas.wesemann@motius.de>

--- tests/vspec/test_generic.py
+++ tests/vspec/test_generic.py
@@ -42,11 +42,10 @@ def run_exporter(directory, exporter, tmp_path):
 
 
 @pytest.mark.parametrize("directory", default_directories(), ids=idfn)
-def test_exporters(directory, tmp_path):
+@pytest.mark.parametrize(
+    "exporter", ["json", "jsonschema", "ddsidl", "csv", "yaml", "franca", "graphql"]
+)
+def test_exporters(directory, tmp_path, exporter: str):
     # Run all "supported" exporters, i.e. not those in contrib
     # Exception is "binary", as it is assumed output may vary depending on target
-    exporters = ["json", "jsonschema", "ddsidl",
-                 "csv", "yaml", "franca", "graphql"]
-
-    for exporter in exporters:
-        run_exporter(directory, exporter, tmp_path)
+    run_exporter(directory, exporter, tmp_path)
