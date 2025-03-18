710a42dab4f1b014ab0c77ba1990bff18211a4fd
Jan Kubovy <jan.kubovy@bmw.de>
2025-03-18 13:05:53 +0100

    Update reviews
    Signed-off-by: Jan Kubovy <jan.kubovy@bmw.de>

--- src/vss_tools/utils/vhal/vehicle_mapping.py
+++ src/vss_tools/utils/vhal/vehicle_mapping.py
@@ -7,58 +7,61 @@
 # SPDX-License-Identifier: MPL-2.0
 
 from dataclasses import dataclass
-from typing import List, Optional
+from typing import List, Optional, Union
+
+from dataclasses_json import LetterCase, dataclass_json
 
 
-@dataclass
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass(frozen=True)
 class VehicleMappingItem:
     """
     Represents a single vehicle mapping item.
     """
 
     name: str
-    propertyId: int
-    areaId: int
+    property_id: int
+    area_id: int
     access: int
-    changeMode: int
+    change_mode: int
     unit: str
     source: str
     formula: Optional[str] = None
     comment: Optional[str] = None
-    configString: Optional[str] = None
+    config_string: Optional[str] = None
 
     datatype: Optional[str] = None
     type: Optional[str] = None
     min: Optional[int] = None
     max: Optional[int] = None
     allowed: Optional[List[str]] = None
-    default: Optional[int] = None
+    default: Optional[Union[Union[int, str], Union[List[int], List[str]]]] = None
     deprecation: Optional[str] = None
 
     @property
-    def vhalGroup(self):
+    def vhal_group(self):
         """
         VHAL group component of the property ID.
         """
-        return (self.propertyId & 0xF0000000) >> 28
+        return (self.property_id & 0xF0000000) >> 28
 
     @property
-    def vhalArea(self):
+    def vhal_area(self):
         """
         VHAL area component of the property ID.
         """
-        return (self.propertyId & 0x0F000000) >> 24
+        return (self.property_id & 0x0F000000) >> 24
 
     @property
-    def vhalType(self):
+    def vhal_type(self):
         """
         VHAL type component of the property ID.
         """
-        return (self.propertyId & 0x00FF0000) >> 16
+        return (self.property_id & 0x00FF0000) >> 16
 
     @property
-    def vhalId(self):
+    def vhal_id(self):
         """
         VHAL unique ID component of the property ID.
         """
-        return self.propertyId & 0x0000FFFF
+        return self.property_id & 0x0000FFFF
