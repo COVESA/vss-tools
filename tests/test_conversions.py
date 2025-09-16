cb6b75fd53e73e209fb707ee9050679532868ca8
Daniel Alvarez-Coello <8550265+jdacoello@users.noreply.github.com>
2025-09-16 23:11:16 +0200

    chore: Update copyright year in test files

--- tests/test_conversions.py
+++ tests/test_conversions.py
@@ -1,6 +1,10 @@
-#!/usr/bin/env python3
-
-"""Test script to debug conversion issues."""
+# Copyright (c) 2025 Contributors to COVESA
+#
+# This program and the accompanying materials are made available under the
+# terms of the Mozilla Public License 2.0 which is available at
+# https://www.mozilla.org/en-US/MPL/2.0/
+#
+# SPDX-License-Identifier: MPL-2.0
 
 import sys
 
--- tests/test_graphql_naming.py
+++ tests/test_graphql_naming.py
@@ -1,6 +1,10 @@
-"""
-Tests for the unified GraphQL naming conventions.
-"""
+# Copyright (c) 2025 Contributors to COVESA
+#
+# This program and the accompanying materials are made available under the
+# terms of the Mozilla Public License 2.0 which is available at
+# https://www.mozilla.org/en-US/MPL/2.0/
+#
+# SPDX-License-Identifier: MPL-2.0
 
 from vss_tools.utils.graphql_utils import (
     GraphQLElementType,
