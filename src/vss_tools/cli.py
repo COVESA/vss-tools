110f2d42e398315ad84b14a0db92cf574607a473
Momin Ali <ali_momin@jp.honda>
2025-10-03 18:36:27 +0900

    Initial commit for adding ros2 exporter in vss-tools (#464)
    Signed-off-by: Ali Momin <ali_momin@jp.honda>
    Signed-off-by: Momin Ali <ali_momin@jp.honda>

--- src/vss_tools/cli.py
+++ src/vss_tools/cli.py
@@ -50,6 +50,7 @@ def cli(ctx: click.Context, log_level: str, log_file: Path):
         "tree": "vss_tools.exporters.tree:cli",
         "samm": "vss_tools.exporters.samm:cli",
         "go": "vss_tools.exporters.go:cli",
+        "ros2interface": "vss_tools.exporters.ros2interface:cli",
     },
 )
 @click.pass_context
