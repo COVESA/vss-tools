ce5dc3405626b5d700fc7af93d6662a2043a633b
Sébastien Roy <sroy@orthogone.com>
2026-09-09 17:17:08 -0400

    Group extra attributes in an x-extra object.t
    Signed-off-by: Sébastien Roy <sroy@orthogone.com>

--- src/vss_tools/exporters/jsonschema.py
+++ src/vss_tools/exporters/jsonschema.py
@@ -152,8 +152,11 @@ def add_node(
         schema["description"] = vss_data.description
     if extend_all_attributes:
         add_x_attributes(schema, node)
-    for field in vss_data.get_extra_attributes():
-        schema[field] = getattr(vss_data, field)
+    extra_attributes = vss_data.get_extra_attributes()
+    if extra_attributes:
+        schema["x-extra"] = {}
+    for field in extra_attributes:
+        schema["x-extra"][field] = getattr(vss_data, field)
     if isinstance(node.data, VSSDataDatatype):
         ref = schema
         if is_array(node.data.datatype):
