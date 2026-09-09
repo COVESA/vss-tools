5d902ab2deb71212efa989d9176c197f02a4704c
Sébastien Roy <sroy@orthogone.com>
2026-09-09 11:29:10 -0400

    Fix trailing whitespaces
    Signed-off-by: Sébastien Roy <sroy@orthogone.com>

--- src/vss_tools/exporters/jsonschema.py
+++ src/vss_tools/exporters/jsonschema.py
@@ -153,7 +153,7 @@ def add_node(
     if extend_all_attributes:
         add_x_attributes(schema, node)
     for field in vss_data.get_extra_attributes():
-        schema[field] = getattr(vss_data, field)        
+        schema[field] = getattr(vss_data, field)
     if isinstance(node.data, VSSDataDatatype):
         ref = schema
         if is_array(node.data.datatype):
